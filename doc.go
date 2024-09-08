6a80abc0f73eb005a4b52af77159e8502fbf1088
PurinPintakhiew <dev.purin@gmail.com>
2024-09-08 22:37:10 +0700

    feat : doc.go

---
+++ doc.go
@@ -0,0 +1,13 @@
+/*
+Package convertunits provides functions to facilitate conversions between various units of measurement, including temperature, weight, and length.
+
+This package is designed to help developers easily convert values from one unit to another with precise calculations, supporting units such as Celsius, Fahrenheit, Kelvin, meters, feet, kilograms, pounds, and more.
+
+Examples of conversions supported:
+  - Temperature: Celsius to Fahrenheit, Kelvin, and more.
+  - Weight: Kilograms to Pounds, Grams, and more.
+  - Length: Meters to Feet, Inches, and more.
+
+The package is suitable for applications in scientific calculations, data processing, and any situation where unit conversion is necessary.
+*/
+package convertunits
