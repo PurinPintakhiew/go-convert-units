c7636f03091723470282f1d127d970b60e102a6d
PurinPintakhiew <dev.purin@gmail.com>
2024-09-12 00:17:24 +0700

    feat: megatonne and gigatonne

--- weight.go
+++ weight.go
@@ -30,3 +30,75 @@ func ConvertWeight(value float64, fromUnit string, toUnit string) float64 {
 
 	return result
 }
+
+func GigatonneToMegatonne(gigatonne float64) float64 {
+	return gigatonne * 1000
+}
+
+func GigatonneToTonne(gigatonne float64) float64 {
+	return gigatonne * 1000000000
+}
+
+func GigatonneToKilogram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000
+}
+
+func GigatonneToPound(gigatonne float64) float64 {
+	return gigatonne * 2204622621848.78
+}
+
+func GigatonneToGram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000000
+}
+
+func GigatonneToMilligram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000000000
+}
+
+func GigatonneToMicrogram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000000000000
+}
+
+func GigatonneToNanogram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000000000000000
+}
+
+func GigatonneToPicogram(gigatonne float64) float64 {
+	return gigatonne * 1000000000000000000000000000
+}
+
+func MegatonneToGigatonne(megatonne float64) float64 {
+	return megatonne * 0.001
+}
+
+func MegatonneToTonne(megatonne float64) float64 {
+	return megatonne * 1000000
+}
+
+func MegatonneToKilogram(megatonne float64) float64 {
+	return megatonne * 1000000000
+}
+
+func MegatonneToPound(megatonne float64) float64 {
+	return megatonne * 2204622621.85
+}
+
+func MegatonneToGram(megatonne float64) float64 {
+	return megatonne * 1000000000000
+}
+
+func MegatonneToMilligram(megatonne float64) float64 {
+	return megatonne * 1000000000000000
+}
+
+func MegatonneToMicrogram(megatonne float64) float64 {
+	return megatonne * 1000000000000000000
+}
+
+func MegatonneToNanogram(megatonne float64) float64 {
+	return megatonne * 1000000000000000000000
+}
+
+func MegatonneToPicogram(megatonne float64) float64 {
+	return megatonne * 1000000000000000000000000
+}
