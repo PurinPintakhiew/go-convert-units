7ebc46f47f5a4de3dd01f0e1df66b7285158419b
PurinPintakhiew <dev.purin@gmail.com>
2024-09-14 22:30:04 +0700

    refactor : Improve code

--- convert/temperature/convert.go
+++ convert/temperature/convert.go
@@ -2,211 +2,157 @@ package temperature
 
 import "fmt"
 
+type tempFunc func(value float64) (float64, error)
+
+var tempConversions = map[string]map[string]tempFunc{
+	"celsius": {
+		"fahrenheit": CelsiusToFahrenheit,
+		"kelvin":     CelsiusToKelvin,
+		"rankine":    CelsiusToRankine,
+		"reaumur":    CelsiusToReaumur,
+	},
+	"fahrenheit": {
+		"celsius": FahrenheitToCelsius,
+		"kelvin":  FahrenheitToKelvin,
+		"rankine": FahrenheitToRankine,
+		"reaumur": FahrenheitToReaumur,
+	},
+	"kelvin": {
+		"celsius":    KelvinToCelsius,
+		"fahrenheit": KelvinToFahrenheit,
+		"rankine":    KelvinToRankine,
+		"reaumur":    KelvinToReaumur,
+	},
+	"rankine": {
+		"celsius":    RankineToCelsius,
+		"fahrenheit": RankineToFahrenheit,
+		"kelvin":     RankineToKelvin,
+		"reaumur":    RankineToReaumur,
+	},
+	"reaumur": {
+		"celsius":    ReaumurToCelsius,
+		"fahrenheit": ReaumurToFahrenheit,
+		"kelvin":     ReaumurToKelvin,
+		"rankine":    ReaumurToRankine,
+	},
+}
+
 // ConvertTemperature converts the given Temperature from one unit to another
-func ConvertTemperature(value float64, fromUnit string, toUnit string) float64 {
-	switch fromUnit {
-	case "celsius":
-		return convertFromCelsius(value, toUnit)
-	case "fahrenheit":
-		return convertFromFahrenheit(value, toUnit)
-	case "kelvin":
-		return convertFromKelvin(value, toUnit)
-	case "rankine":
-		return convertFromRankine(value, toUnit)
-	case "reaumur":
-		return convertFromReaumur(value, toUnit)
-	default:
-		fmt.Printf("Invalid units: %s\n", fromUnit)
-		return 0.0
+func ConvertTemperature(value float64, fromUnit string, toUnit string) (float64, error) {
+	fromConversions, fromExists := tempConversions[fromUnit]
+	if !fromExists {
+		return 0, fmt.Errorf("invalid from unit: %s", fromUnit)
 	}
-}
 
-func convertFromCelsius(value float64, toUnit string) float64 {
-	switch toUnit {
-	case "fahrenheit":
-		return CelsiusToFahrenheit(value)
-	case "kelvin":
-		return CelsiusToKelvin(value)
-	case "rankine":
-		return CelsiusToRankine(value)
-	case "reaumur":
-		return CelsiusToReaumur(value)
-	case "celsius":
-		return value
-	default:
-		fmt.Printf("Invalid units: %s\n", toUnit)
-		return 0.0
+	toConversion, toExists := fromConversions[toUnit]
+	if !toExists {
+		return 0, fmt.Errorf("invalid to unit: %s", toUnit)
 	}
-}
 
-func convertFromFahrenheit(value float64, toUnit string) float64 {
-	switch toUnit {
-	case "celsius":
-		return FahrenheitToCelsius(value)
-	case "kelvin":
-		return FahrenheitToKelvin(value)
-	case "rankine":
-		return FahrenheitToRankine(value)
-	case "reaumur":
-		return FahrenheitToReaumur(value)
-	case "fahrenheit":
-		return value
-	default:
-		fmt.Printf("Invalid units: %s\n", toUnit)
-		return 0.0
+	result, err := toConversion(value)
+	if err != nil {
+		return 0, err
 	}
-}
 
-func convertFromKelvin(value float64, toUnit string) float64 {
-	switch toUnit {
-	case "celsius":
-		return KelvinToCelsius(value)
-	case "fahrenheit":
-		return KelvinToFahrenheit(value)
-	case "rankine":
-		return KelvinToRankine(value)
-	case "reaumur":
-		return KelvinToReaumur(value)
-	case "kelvin":
-		return value
-	default:
-		fmt.Printf("Invalid units: %s\n", toUnit)
-		return 0.0
-	}
-}
-
-func convertFromRankine(value float64, toUnit string) float64 {
-	switch toUnit {
-	case "celsius":
-		return RankineToCelsius(value)
-	case "fahrenheit":
-		return RankineToFahrenheit(value)
-	case "kelvin":
-		return RankineToKelvin(value)
-	case "reaumur":
-		return RankineToReaumur(value)
-	case "rankine":
-		return value
-	default:
-		fmt.Printf("Invalid units: %s\n", toUnit)
-		return 0.0
-	}
-}
-
-func convertFromReaumur(value float64, toUnit string) float64 {
-	switch toUnit {
-	case "celsius":
-		return ReaumurToCelsius(value)
-	case "fahrenheit":
-		return ReaumurToFahrenheit(value)
-	case "kelvin":
-		return ReaumurToKelvin(value)
-	case "rankine":
-		return ReaumurToRankine(value)
-	case "reaumur":
-		return value
-	default:
-		fmt.Printf("Invalid units: %s\n", toUnit)
-		return 0.0
-	}
+	return result, nil
 }
 
 // Convert Celsius to Fahrenheit
-func CelsiusToFahrenheit(celsius float64) float64 {
-	return (celsius * 9 / 5) + 32
+func CelsiusToFahrenheit(celsius float64) (float64, error) {
+	return (celsius * 9 / 5) + 32, nil
 }
 
 // Convert Celsius to Kelvin
-func CelsiusToKelvin(celsius float64) float64 {
-	return celsius + 273.15
+func CelsiusToKelvin(celsius float64) (float64, error) {
+	return celsius + 273.15, nil
 }
 
 // Convert Celsius to Rankine
-func CelsiusToRankine(celsius float64) float64 {
-	return (celsius + 273.15) * 9 / 5
+func CelsiusToRankine(celsius float64) (float64, error) {
+	return (celsius + 273.15) * 9 / 5, nil
 }
 
 // Convert Celsius to Reaumur
-func CelsiusToReaumur(celsius float64) float64 {
-	return celsius * 4 / 5
+func CelsiusToReaumur(celsius float64) (float64, error) {
+	return celsius * 4 / 5, nil
 }
 
 // Convert Fahrenheit to Celsius
-func FahrenheitToCelsius(fahrenheit float64) float64 {
-	return (fahrenheit - 32) * 5 / 9
+func FahrenheitToCelsius(fahrenheit float64) (float64, error) {
+	return (fahrenheit - 32) * 5 / 9, nil
 }
 
 // Convert Fahrenheit to Kelvin
-func FahrenheitToKelvin(fahrenheit float64) float64 {
-	return (fahrenheit-32)*5/9 + 273.15
+func FahrenheitToKelvin(fahrenheit float64) (float64, error) {
+	return (fahrenheit-32)*5/9 + 273.15, nil
 }
 
 // Convert Fahrenheit to Rankine
-func FahrenheitToRankine(fahrenheit float64) float64 {
-	return fahrenheit + 459.67
+func FahrenheitToRankine(fahrenheit float64) (float64, error) {
+	return fahrenheit + 459.67, nil
 }
 
 // Convert Fahrenheit to Reaumur
-func FahrenheitToReaumur(fahrenheit float64) float64 {
-	return (fahrenheit - 32) * 4 / 9
+func FahrenheitToReaumur(fahrenheit float64) (float64, error) {
+	return (fahrenheit - 32) * 4 / 9, nil
 }
 
 // Convert Kelvin to Celsius
-func KelvinToCelsius(kelvin float64) float64 {
-	return kelvin - 273.15
+func KelvinToCelsius(kelvin float64) (float64, error) {
+	return kelvin - 273.15, nil
 }
 
 // Convert Kelvin to Fahrenheit
-func KelvinToFahrenheit(kelvin float64) float64 {
-	return (kelvin-273.15)*9/5 + 32
+func KelvinToFahrenheit(kelvin float64) (float64, error) {
+	return (kelvin-273.15)*9/5 + 32, nil
 }
 
 // Convert Kelvin to Rankine
-func KelvinToRankine(kelvin float64) float64 {
-	return kelvin * 9 / 5
+func KelvinToRankine(kelvin float64) (float64, error) {
+	return kelvin * 9 / 5, nil
 }
 
 // Convert Kelvin to Reaumur
-func KelvinToReaumur(kelvin float64) float64 {
-	return (kelvin - 273.15) * 4 / 5
+func KelvinToReaumur(kelvin float64) (float64, error) {
+	return (kelvin - 273.15) * 4 / 5, nil
 }
 
 // Convert Rankine to Celsius
-func RankineToCelsius(rankine float64) float64 {
-	return (rankine - 491.67) * 5 / 9
+func RankineToCelsius(rankine float64) (float64, error) {
+	return (rankine - 491.67) * 5 / 9, nil
 }
 
 // Convert Rankine to Fahrenheit
-func RankineToFahrenheit(rankine float64) float64 {
-	return rankine - 459.67
+func RankineToFahrenheit(rankine float64) (float64, error) {
+	return rankine - 459.67, nil
 }
 
 // Convert Rankine to Kelvin
-func RankineToKelvin(rankine float64) float64 {
-	return rankine * 5 / 9
+func RankineToKelvin(rankine float64) (float64, error) {
+	return rankine * 5 / 9, nil
 }
 
 // Convert Rankine to Reaumur
-func RankineToReaumur(rankine float64) float64 {
-	return (rankine - 491.67) * 4 / 9
+func RankineToReaumur(rankine float64) (float64, error) {
+	return (rankine - 491.67) * 4 / 9, nil
 }
 
 // Convert Reaumur to Celsius
-func ReaumurToCelsius(reaumur float64) float64 {
-	return reaumur * 5 / 4
+func ReaumurToCelsius(reaumur float64) (float64, error) {
+	return reaumur * 5 / 4, nil
 }
 
 // Convert Reaumur to Fahrenheit
-func ReaumurToFahrenheit(reaumur float64) float64 {
-	return (reaumur * 9 / 4) + 32
+func ReaumurToFahrenheit(reaumur float64) (float64, error) {
+	return (reaumur * 9 / 4) + 32, nil
 }
 
 // Convert Reaumur to Kelvin
-func ReaumurToKelvin(reaumur float64) float64 {
-	return (reaumur * 5 / 4) + 273.15
+func ReaumurToKelvin(reaumur float64) (float64, error) {
+	return (reaumur * 5 / 4) + 273.15, nil
 }
 
 // Convert Reaumur to Rankine
-func ReaumurToRankine(reaumur float64) float64 {
-	return (reaumur * 9 / 4) + 491.67
+func ReaumurToRankine(reaumur float64) (float64, error) {
+	return (reaumur * 9 / 4) + 491.67, nil
 }
